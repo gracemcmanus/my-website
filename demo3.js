a44786c31b90d3c6e7a62c87dca6f058857114e4
Grace McManus <grace.mcmanus@verizon.net>
2016-10-20 20:52:27 -0400

    added js files

---
+++ demo3.js
@@ -0,0 +1,6 @@
+function myFunction1() {
+    debugger;
+    document.getElementById("demo3").innerHTML = "Javascript changed this paragraph text";
+    document.getElementById("demo3").style.fontSize = "25px";
+    document.getElementById("demo3").style.color = "blue";
+}
